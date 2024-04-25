b0e344475542529df364872551e9042d352484ee
Kamil <Kamil@DESKTOP-1VC03VA>
2024-04-25 10:17:49 +0200

    Dodaj pliki projektów.

---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,34 @@
+﻿public static class QuickSort
+{
+    public static void Sortuj(int[] tab, int lewy, int prawy)
+    {
+        int i = lewy, j = prawy;
+        int pivot = tab[(lewy + prawy) / 2];
+        int tmp;
+        while (i <= j)
+        {
+            while (tab[i] < pivot)
+                i++;
+            while (tab[j] > pivot)
+                j--;
+            if (i <= j)
+            {
+                tmp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+
+        if (lewy < j)
+            Sortuj(tab, lewy, j);
+        if (i < prawy)
+            Sortuj(tab, i, prawy);
+    }
+
+    public static void Sortuj(int[] tab)
+    {
+        Sortuj(tab, 0, tab.Length - 1);
+    }
+}
---
+++ Testsortowania/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static QuickSort; 
+
+class Program
+{
+    static void Main()
+    {
+        int[] liczby = new int[10];
+        Random r = new Random();
+        Console.WriteLine("Tablica przed sortowaniem: ");
+        for (int i = 0; i < liczby.Length; i++)
+        {
+            liczby[i] = r.Next(0, 1001);
+            Console.Write(liczby[i] + ", ");
+        }
+
+        QuickSort.Sortuj(liczby);
+        Console.WriteLine("\n\nTablica po sortowaniu: ");
+        foreach (int liczba in liczby)
+        {
+            Console.Write(liczba + ", ");
+        }
+        Console.ReadKey();
+    }
+}
+
